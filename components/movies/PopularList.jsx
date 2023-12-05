9f239033208c0e96e50f5d40e7f415fcd2692f7d
Nysa <naominysa@gmail.com>
2023-12-05 12:27:11 -0700

    push

--- components/movies/PopularList.jsx
+++ components/movies/PopularList.jsx
@@ -1,27 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+
+import { CCarousel, CCarouselItem, CImage  } from "@coreui/react";
 
 const PopularList = () => {
-    const [movies, setMovies] = useState([])
-    const fetchData = async () => {
-        fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzQ4YzAyZDdlNzAxNDE3YzVmN2QyYzY2OTQ1MjY5MCIsInN1YiI6IjY1NmU1MTQ4M2RjMzEzMDBhY2FiZDk1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P44TLiXVMTyw1Sp2Hi9OhE4dz0IxS0XB6_tY6dX8M0U'
-        }
-      }).then(res => res.json()).then(data => {
-        console.log(data);
-        setMovies(data);
-      })
-    }
+  const [movies, setMovies] = useState([]);
+  const fetchData = async () => {
+    fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzQ4YzAyZDdlNzAxNDE3YzVmN2QyYzY2OTQ1MjY5MCIsInN1YiI6IjY1NmU1MTQ4M2RjMzEzMDBhY2FiZDk1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P44TLiXVMTyw1Sp2Hi9OhE4dz0IxS0XB6_tY6dX8M0U",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setMovies(data.results);
+      });
+  };
 
-    useEffect(() => {
-      fetchData();
-    }, [])
-    
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>PopularList</div>
-  )
-}
+    <CCarousel indicators interval="1000">
+      {movies.length ? (
+        movies.map((m, index) => {
+          if (index < 5) {
+            return (
+              <CCarouselItem key={m.id}>
+                <CImage 
+                 style={{ width: "100vw", opacity: 0.6}}
+                 
+                  src={`https://image.tmdb.org/t/p/original${m.backdrop_path}`}
+                />
+              </CCarouselItem>
+            );
+          }
+        })
+      ) : (
+        <p></p>
+      )}
+    </CCarousel>
+  );
+};
 
-export default PopularList
+export default PopularList;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Movies from "@/components/movies/Movies";
 import Shows from "@/components/shows/Shows";
 import { useState } from "react";
+import '@coreui/coreui/dist/css/coreui.min.css'
 
 export default function Home() {
 const [showMovies, setShowMovies] = useState(true);
@@ -8,8 +9,8 @@ const [showMovies, setShowMovies] = useState(true);
   return (
     <>
       <nav>
-      <button onClick={() => setShowMovies(false)}>Shows</button>
       <button onClick={() => setShowMovies(true)}>Movies</button>
+      <button onClick={() => setShowMovies(false)}>Shows</button>
       </nav>
       <div>{showMovies ? <Movies /> : <Shows />}</div>
     </>
